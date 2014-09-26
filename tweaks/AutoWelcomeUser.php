1146202d687b4a2d94cf9ee28318bfd472a29439
lethosor <lethosor@gmail.com>
2014-09-26 15:52:03 -0400

    Only welcome users that have made a given number of edits
    Resolves #7

--- tweaks/AutoWelcomeUser.php
+++ tweaks/AutoWelcomeUser.php
@@ -6,21 +6,29 @@
 
 $wgAutoWelcomeUserText = 'Welcome to this wiki, {{subst:PAGENAME}}!';
 $wgAutoWelcomeUserAuthor = 'AutoWelcomeUser';
+$wgAutoWelcomeUserEditCount = 1;
 
 $wgExtensionCredits['AutoWelcomeUser'][] = array(
     'path' => __FILE__,
     'name' => 'AutoWelcomeUser',
     'author' =>'Lethosor',
     'url' => 'https://github.com/lethosor/DFWikiFunctions',
-    'description' => 'Posts a message to new users\' talk pages upon user creation',
-    'version'  => '0.1',
+    'description' => 'Posts a message to new users\' talk pages',
+    'version'  => '0.2',
 );
 
 class AutoWelcomeUserHooks {
-    public static function AddNewAccount ($user, $byEmail) {
-        global $wgAutoWelcomeUserText, $wgAutoWelcomeUserAuthor;
+    public static function PageContentSaveComplete ($article, $user /* , ... */) {
+        global $wgAutoWelcomeUserText, $wgAutoWelcomeUserAuthor, $wgAutoWelcomeUserEditCount;
+        if ($user->getEditCount() != $wgAutoWelcomeUserEditCount - 1) {
+            // Edit count is not increased until after this hook runs
+            return true;
+        }
         $page = WikiPage::factory($user->getTalkPage());
         $author = User::newFromName($wgAutoWelcomeUserAuthor);
+        // Avoid sending welcome messages to welcoming user
+        if ($user->getName() == $author->getName())
+            return true;
         $page->doEdit(
             $wgAutoWelcomeUserText,
             "Welcoming user " . $user->getName(),
@@ -30,4 +38,4 @@ class AutoWelcomeUserHooks {
         return true;
     }
 }
-$wgHooks['AddNewAccount'][] = 'AutoWelcomeUserHooks::AddNewAccount';
+$wgHooks['PageContentSaveComplete'][] = 'AutoWelcomeUserHooks::PageContentSaveComplete';
