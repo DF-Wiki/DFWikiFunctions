e40cafa4a212cdf63fcdea18bde65ed6325fa283
lethosor <lethosor@gmail.com>
2015-05-08 21:01:00 -0400

    Expand RandomByNamespace to include all namespaces

--- extensions/RandomByNamespace/SpecialRandomByNamespace.php
+++ extensions/RandomByNamespace/SpecialRandomByNamespace.php
@@ -4,11 +4,15 @@ class SpecialRandomByNamespace extends SpecialPage {
         parent::__construct('RandomByNamespace');
     }
     public function execute($par) {
-        global $wgExtraNamespaces;
+        $namespaces = MWNamespace::getCanonicalNamespaces();
         $request = $this->getRequest();
         $output = $this->getOutput();
         $output->setPageTitle($this->msg('randombynamespace')->parse());
-        foreach ($wgExtraNamespaces as $id => $ns) {
+        foreach ($namespaces as $id => $ns) {
+            if ($id < 0)
+                continue;
+            if ($ns == '')
+                $ns = 'Main';
             $ns = str_replace('_', ' ', $ns);
             $output->addWikiText("*[[Special:Random/$ns|$ns]]\n");
         }
